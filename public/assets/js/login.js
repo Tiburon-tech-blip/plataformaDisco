a603f43f2cf3456c27950ba11c96e02075c2eeb2
Alexis <130183158+Tiburon-tech-blip@users.noreply.github.com>
2024-11-06 20:50:29 -0300

    Add files via upload

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -14,7 +14,7 @@ loginForm.addEventListener('submit', async (event) => {
   //validacion()
 
   try {
-    const response = await fetch('/login', {
+    const response = await fetch('https://plataformadisco-przk.onrender.com/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
